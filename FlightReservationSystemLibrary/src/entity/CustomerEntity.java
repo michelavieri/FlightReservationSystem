5f58787b03a0a866a1fcc696078e9337613b9605
michelavieri <michelavieri@u.nus.edu>
2020-10-25 17:01:05 +0800

    Modified CustomerEntity

--- FlightReservationSystemLibrary/src/entity/CustomerEntity.java
+++ FlightReservationSystemLibrary/src/entity/CustomerEntity.java
@@ -21,31 +21,31 @@ public class CustomerEntity implements Serializable {
     private static final long serialVersionUID = 1L;
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+    private Long customerId;
 
-    public Long getId() {
-        return id;
+    public Long getCustomerId() {
+        return customerId;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
     }
 
     @Override
     public int hashCode() {
         int hash = 0;
-        hash += (id != null ? id.hashCode() : 0);
+        hash += (customerId != null ? customerId.hashCode() : 0);
         return hash;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
+        // TODO: Warning - this method won't work in the case the customerId fields are not set
         if (!(object instanceof CustomerEntity)) {
             return false;
         }
         CustomerEntity other = (CustomerEntity) object;
-        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+        if ((this.customerId == null && other.customerId != null) || (this.customerId != null && !this.customerId.equals(other.customerId))) {
             return false;
         }
         return true;
@@ -53,7 +53,7 @@ public class CustomerEntity implements Serializable {
 
     @Override
     public String toString() {
-        return "entity.CustomerEntity[ id=" + id + " ]";
+        return "entity.CustomerEntity[ id=" + customerId + " ]";
     }
     
 }
