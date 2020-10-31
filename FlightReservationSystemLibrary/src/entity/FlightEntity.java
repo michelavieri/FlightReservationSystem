62ea910eaa54c1fdbcbb63de9be83a8f0d1172eb
michelavieri <michelavieri@u.nus.edu>
2020-10-31 11:45:21 +0800

    Initialize Entity: - Airport - Flight - FlightRoute - FlightSchedule - FlightSchedulePlan - Partner - SeatsInventory Initialized util.enumeration folder and enumeration: - FlightSchedulePlanTypeEnum

---
+++ FlightReservationSystemLibrary/src/entity/FlightEntity.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+/**
+ *
+ * @author miche
+ */
+@Entity
+public class FlightEntity implements Serializable {
+
+    @OneToMany(mappedBy = "flight")
+    private List<FlightSchedulePlanEntity> flightSchedulePlans;
+
+    @OneToMany(mappedBy = "flight")
+    private List<SeatsInventoryEntity> seatsInventories;
+    
+    @ManyToOne
+    private FlightRouteEntity route;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String flightCode;
+    private boolean disabled;
+    @OneToOne(mappedBy = "departureFlight")
+    private FlightEntity returnFlight;
+    @OneToOne
+    private FlightEntity departureFlight;
+
+    public String getFlightCode() {
+        return flightCode;
+    }
+
+    public void setFlightCode(String flightCode) {
+        this.flightCode = flightCode;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (flightCode != null ? flightCode.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the flightCode fields are not set
+        if (!(object instanceof FlightEntity)) {
+            return false;
+        }
+        FlightEntity other = (FlightEntity) object;
+        if ((this.flightCode == null && other.flightCode != null) || (this.flightCode != null && !this.flightCode.equals(other.flightCode))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.FlightEntity[ id=" + flightCode + " ]";
+    }
+
+    /**
+     * @return the disabled
+     */
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    /**
+     * @param disabled the disabled to set
+     */
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    /**
+     * @return the returnFlight
+     */
+    public FlightEntity getReturnFlight() {
+        return returnFlight;
+    }
+
+    /**
+     * @param returnFlight the returnFlight to set
+     */
+    public void setReturnFlight(FlightEntity returnFlight) {
+        this.returnFlight = returnFlight;
+    }
+
+    /**
+     * @return the departureFlight
+     */
+    public FlightEntity getDepartureFlight() {
+        return departureFlight;
+    }
+
+    /**
+     * @param departureFlight the departureFlight to set
+     */
+    public void setDepartureFlight(FlightEntity departureFlight) {
+        this.departureFlight = departureFlight;
+    }
+
+    /**
+     * @return the seatsInventories
+     */
+    public List<SeatsInventoryEntity> getSeatsInventories() {
+        return seatsInventories;
+    }
+
+    /**
+     * @param seatsInventories the seatsInventories to set
+     */
+    public void setSeatsInventories(List<SeatsInventoryEntity> seatsInventories) {
+        this.seatsInventories = seatsInventories;
+    }
+
+    /**
+     * @return the route
+     */
+    public FlightRouteEntity getRoute() {
+        return route;
+    }
+
+    /**
+     * @param route the route to set
+     */
+    public void setRoute(FlightRouteEntity route) {
+        this.route = route;
+    }
+
+    /**
+     * @return the flightSchedulePlans
+     */
+    public List<FlightSchedulePlanEntity> getFlightSchedulePlans() {
+        return flightSchedulePlans;
+    }
+
+    /**
+     * @param flightSchedulePlans the flightSchedulePlans to set
+     */
+    public void setFlightSchedulePlans(List<FlightSchedulePlanEntity> flightSchedulePlans) {
+        this.flightSchedulePlans = flightSchedulePlans;
+    }
+
+}
---
+++ FlightReservationSystemLibrary/src/entity/FlightSchedulePlanEntity.java
@@ -0,0 +1,174 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import util.enumeration.FlightSchedulePlanTypeEnum;
+
+/**
+ *
+ * @author miche
+ */
+@Entity
+public class FlightSchedulePlanEntity implements Serializable {
+
+    @OneToMany(mappedBy = "plan")
+    private List<FlightScheduleEntity> flightSchedules;
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long schedulePlanId;
+    private FlightSchedulePlanTypeEnum type;
+    private Date startDate;
+    private Date endDate;
+    private boolean disabled;
+    private int layoverDuration;
+    
+    @ManyToOne
+    private FlightEntity flight;
+    
+
+    public Long getSchedulePlanId() {
+        return schedulePlanId;
+    }
+
+    public void setSchedulePlanId(Long schedulePlanId) {
+        this.schedulePlanId = schedulePlanId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (schedulePlanId != null ? schedulePlanId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the schedulePlanId fields are not set
+        if (!(object instanceof FlightSchedulePlanEntity)) {
+            return false;
+        }
+        FlightSchedulePlanEntity other = (FlightSchedulePlanEntity) object;
+        if ((this.schedulePlanId == null && other.schedulePlanId != null) || (this.schedulePlanId != null && !this.schedulePlanId.equals(other.schedulePlanId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.FlightSchedulePlanEntity[ id=" + schedulePlanId + " ]";
+    }
+
+    /**
+     * @return the type
+     */
+    public FlightSchedulePlanTypeEnum getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(FlightSchedulePlanTypeEnum type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the startDate
+     */
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    /**
+     * @param startDate the startDate to set
+     */
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    /**
+     * @return the endDate
+     */
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    /**
+     * @param endDate the endDate to set
+     */
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    /**
+     * @return the disabled
+     */
+    public boolean isDisabled() {
+        return disabled;
+    }
+
+    /**
+     * @param disabled the disabled to set
+     */
+    public void setDisabled(boolean disabled) {
+        this.disabled = disabled;
+    }
+
+    /**
+     * @return the layoverDuration
+     */
+    public int getLayoverDuration() {
+        return layoverDuration;
+    }
+
+    /**
+     * @param layoverDuration the layoverDuration to set
+     */
+    public void setLayoverDuration(int layoverDuration) {
+        this.layoverDuration = layoverDuration;
+    }
+
+    /**
+     * @return the flightSchedules
+     */
+    public List<FlightScheduleEntity> getFlightSchedules() {
+        return flightSchedules;
+    }
+
+    /**
+     * @param flightSchedules the flightSchedules to set
+     */
+    public void setFlightSchedules(List<FlightScheduleEntity> flightSchedules) {
+        this.flightSchedules = flightSchedules;
+    }
+
+    /**
+     * @return the flight
+     */
+    public FlightEntity getFlight() {
+        return flight;
+    }
+
+    /**
+     * @param flight the flight to set
+     */
+    public void setFlight(FlightEntity flight) {
+        this.flight = flight;
+    }
+    
+}
