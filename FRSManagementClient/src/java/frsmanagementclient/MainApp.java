2d2389b1f27f815df37878e9f6b65943217b8a55
michelavieri <michelavieri@u.nus.edu>
2020-11-01 19:09:28 +0800

    Create aircraft configuration and other menus in client

--- FRSManagementClient/src/java/frsmanagementclient/MainApp.java
+++ FRSManagementClient/src/java/frsmanagementclient/MainApp.java
@@ -6,7 +6,11 @@
 package frsmanagementclient;
 
 import ejb.session.stateless.EmployeeEntitySessionBeanRemote;
+import entity.AircraftTypeEntity;
+import entity.CabinClassConfigurationEntity;
 import entity.EmployeeEntity;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import util.enumeration.UserRoleEnum;
 import util.exception.InvalidLoginCredentialException;
@@ -102,13 +106,11 @@ public class MainApp {
 
         while (true) {
             System.out.println("*** FRS Management Application ***");
-            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getRole().toString() + " rights");
-            System.out.println("1: Create Customer");
-            System.out.println("2: Open Deposit Account");
-            System.out.println("3: Issue ATM Card");
-            System.out.println("4: Issue Replacement ATM Card");
-            System.out.println("5: Create Employee");
-            System.out.println("6: Logout");
+            System.out.println("You are login as " + currentEmployee.getName() + " with Fleet Manager rights");
+            System.out.println("1: Create Aircraft Configuration");
+            System.out.println("2: View All Aircraft Configurations");
+            System.out.println("3: View Aircraft Configuration Details");
+            System.out.println("4: Logout");
             response = 0;
 
             while (response < 1 || response > 6) {
@@ -116,7 +118,67 @@ public class MainApp {
                 response = sc.nextInt();
 
                 if (response == 1) {
+                    System.out.println("*** FRS Fleet Manager :: Create New Aircraft Configuration ***");
+                    sc.nextLine();
+                    System.out.println("Here are the available lists of aircraft types:")
+                    List<AircraftTypeEntity> types = aircraftTypeEntitySessionBean.retrieveAllTypes();
+                    for (AircraftTypeEntity a : types) {
+                        System.out.println("- " + a);
+                    }
+                    System.out.print("Enter aircraft type> ");
+                    String type = sc.nextLine();
+                    System.out.print("Enter aircraft code> ");
+                    String code = sc.nextLine();
+                    System.out.print("Enter aircraft name> ");
+                    String name = sc.nextLine();
+                    int numOfClasses = 0;
+                    while (numOfClasses < 1 || numOfClasses > 4) {
+                        System.out.print("Enter number of cabin classes (1-4)> ");
+                        numOfClasses = sc.nextInt();
+                        if (numOfClasses < 1 || numOfClasses > 4) {
+                            System.out.println("Please enter a number from 1-4!");
+                        }
+                    }
+                    List<CabinClassConfigurationEntity> classes = new ArrayList<CabinClassConfigurationEntity>();
+                    System.out.println("Here are the list of cabin class types:");
+                    System.out.println("1: First Class");
+                    System.out.println("2: Business Class");
+                    System.out.println("3: Premium Economy Class");
+                    System.out.println("4: Economy Class");
+                    System.out.println("Enter " + numOfClasses + " cabin classes based on the numbers above:");
+                    for (int i = 1; i <= numOfClasses; i++) {
+                        int response2 = 0;
+                        CabinClassTypeEnum classType = CabinClassTypeEnum.FIRST_CLASS;
+                        while (response2 < 1 || response2 > 4) {
+                            System.out.print(i + "> ");
+                            if (response2 == 1) {
+                                classType = CabinClassTypeEnum.FIRST_CLASS;
+                            } else if (response2 == 2) {
+                                classType = CabinClassTypeEnum.BUSINESS_CLASS;
+                            } else if (response2 == 3) {
+                                classType = CabinClassTypeEnum.PREMIUM_ECONOMY_CLASS;
+                            } else if (response2 == 4) {
+                                classType = CabinClassTypeEnum.ECONOMY_CLASS;
+                            } else {
+                                System.out.println("Please enter numbers from 1-4!");
+                            }
 
+                            System.out.println("***Create Cabin Class Configuration***");
+                            System.out.print("Enter number of aisles> ");
+                            int aisles = sc.nextInt();
+                            System.out.print("Enter number of rows> ");
+                            int rows = sc.nextInt();
+                            System.out.print("Enter number of seat abreast> ");
+                            int abreast = sc.nextInt();
+                            System.out.print("Enter maximum capacity> ");
+                            int maxCapacity = sc.nextInt();
+                            classes.add(cabinClassConfigurationSessionBeanRemote.
+                                    createNewCabinClassConfiguration(
+                                            new CabinClassConfigurationEntity(aisles, rows, abreast, maxCapacity, classType)));
+                        }
+                    }
+
+                    aircraftConfigurationEntitySessionBeanRemote.createAircraftConfiguration(code, name, numOfClasses, type, classes);
                 } else if (response == 2) {
 
                 } else if (response == 3) {
@@ -125,8 +187,10 @@ public class MainApp {
 
                 } else if (response == 5) {
 
-                } else {
+                } else if (response == 6) {
                     break;
+                } else {
+                    System.out.println("Invalid response!");
                 }
             }
             if (response == 6) {
@@ -140,13 +204,11 @@ public class MainApp {
 
         while (true) {
             System.out.println("*** FRS Management Application ***");
-            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
-            System.out.println("1: Create Customer");
-            System.out.println("2: Open Deposit Account");
-            System.out.println("3: Issue ATM Card");
-            System.out.println("4: Issue Replacement ATM Card");
-            System.out.println("5: Create Employee");
-            System.out.println("6: Logout");
+            System.out.println("You are login as " + currentEmployee.getName() + " with Route Planner rights");
+            System.out.println("1: Create Flight Route");
+            System.out.println("2: View All Flight Routes");
+            System.out.println("3: Delete Flight Route");
+            System.out.println("4: Logout");
             response = 0;
 
             while (response < 1 || response > 6) {
@@ -160,11 +222,9 @@ public class MainApp {
                 } else if (response == 3) {
 
                 } else if (response == 4) {
-
-                } else if (response == 5) {
-
-                } else {
                     break;
+                } else {
+                    System.out.println("Invalid response!");
                 }
             }
             if (response == 6) {
@@ -178,13 +238,14 @@ public class MainApp {
 
         while (true) {
             System.out.println("*** FRS Management Application ***");
-            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
-            System.out.println("1: Create Customer");
-            System.out.println("2: Open Deposit Account");
-            System.out.println("3: Issue ATM Card");
-            System.out.println("4: Issue Replacement ATM Card");
-            System.out.println("5: Create Employee");
-            System.out.println("6: Logout");
+            System.out.println("You are login as " + currentEmployee.getName() + " with Schedule Manager rights");
+            System.out.println("1: Create Flight");
+            System.out.println("2: View All Flights");
+            System.out.println("3: View Flight Details");
+            System.out.println("4: Create Flight Schedule Plan");
+            System.out.println("5: View All Flight Schedule Plans");
+            System.out.println("6: View Flight Schedule Plan Details");
+            System.out.println("7: Logout");
             response = 0;
 
             while (response < 1 || response > 6) {
@@ -201,8 +262,12 @@ public class MainApp {
 
                 } else if (response == 5) {
 
-                } else {
+                } else if (response == 6) {
+
+                } else if (response == 7) {
                     break;
+                } else {
+                    System.out.println("Invalid response!");
                 }
             }
             if (response == 6) {
@@ -216,7 +281,7 @@ public class MainApp {
 
         while (true) {
             System.out.println("*** FRS Management Application ***");
-            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
+            System.out.println("You are login as " + currentEmployee.getName() + " with Sales Manager rights");
             System.out.println("1: View Seats Inventory");
             System.out.println("2: View Flight Reservations");
             System.out.println("3: Logout");
@@ -232,9 +297,11 @@ public class MainApp {
 
                 } else if (response == 3) {
                     break;
+                } else {
+                    System.out.println("Invalid response!");
                 }
             }
-            if (response == 6) {
+            if (response == 3) {
                 break;
             }
         }
@@ -253,6 +320,8 @@ public class MainApp {
 
                 if (response == 1) {
                     break;
+                } else {
+                    System.out.println("Invalid response!");
                 }
             }
             if (response == 1) {
