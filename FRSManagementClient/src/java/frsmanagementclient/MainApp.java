a358818c809642801136e85001107c4039d9186e
michelavieri <michelavieri@u.nus.edu>
2020-11-01 16:18:03 +0800

    update client management

--- FRSManagementClient/src/java/frsmanagementclient/MainApp.java
+++ FRSManagementClient/src/java/frsmanagementclient/MainApp.java
@@ -8,6 +8,7 @@ package frsmanagementclient;
 import ejb.session.stateless.EmployeeEntitySessionBeanRemote;
 import entity.EmployeeEntity;
 import java.util.Scanner;
+import util.enumeration.UserRoleEnum;
 import util.exception.InvalidLoginCredentialException;
 import util.exception.InvalidUsernameException;
 import util.exception.WrongPasswordException;
@@ -24,7 +25,7 @@ public class MainApp {
     
     public MainApp() {
     }
-    
+
     public void runApp() {
         Scanner sc = new Scanner(System.in);
         Integer response = 0;
@@ -42,7 +43,7 @@ public class MainApp {
                     try {
                         doLogin(sc);
                         System.out.println("Login successful!");
-                        
+
                         menuMain(sc);
                     } catch (InvalidUsernameException | WrongPasswordException | InvalidLoginCredentialException ex) {
                         System.out.println("Invalid login credential: " + ex.getMessage());
@@ -58,7 +59,7 @@ public class MainApp {
             }
         }
     }
-    
+
     public void doLogin(Scanner sc) throws InvalidUsernameException, WrongPasswordException, InvalidLoginCredentialException {
         System.out.println("*** FRS Employee :: Login ***");
         sc.nextLine();
@@ -79,8 +80,24 @@ public class MainApp {
             throw new InvalidLoginCredentialException("Missing login credential!");
         }
     }
-    
+
     public void menuMain(Scanner sc) {
+        UserRoleEnum role = currentEmployee.getRole();
+
+        if (role == UserRoleEnum.FLEET_MANAGER) {
+            menuMainFleet(sc);
+        } else if (role == UserRoleEnum.ROUTE_PLANNER) {
+            menuMainRoute(sc);
+        } else if (role == UserRoleEnum.SCHEDULE_MANAGER) {
+            menuMainSchedule(sc);
+        } else if (role == UserRoleEnum.SALES_MANAGER) {
+            menuMainSales(sc);
+        } else {
+            menuMainEmployee(sc);
+        }
+    }
+
+    public void menuMainFleet(Scanner sc) {
         Integer response = 0;
 
         while (true) {
@@ -99,20 +116,146 @@ public class MainApp {
                 response = sc.nextInt();
 
                 if (response == 1) {
-                    
+
                 } else if (response == 2) {
-                    
+
                 } else if (response == 3) {
-                    
+
                 } else if (response == 4) {
-                    
+
                 } else if (response == 5) {
-                    
+
                 } else {
                     break;
                 }
             }
             if (response == 6) {
+                break;
+            }
+        }
+    }
+
+    public void menuMainRoute(Scanner sc) {
+        Integer response = 0;
+
+        while (true) {
+            System.out.println("*** FRS Management Application ***");
+            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
+            System.out.println("1: Create Customer");
+            System.out.println("2: Open Deposit Account");
+            System.out.println("3: Issue ATM Card");
+            System.out.println("4: Issue Replacement ATM Card");
+            System.out.println("5: Create Employee");
+            System.out.println("6: Logout");
+            response = 0;
+
+            while (response < 1 || response > 6) {
+                System.out.print("> ");
+                response = sc.nextInt();
+
+                if (response == 1) {
+
+                } else if (response == 2) {
+
+                } else if (response == 3) {
+
+                } else if (response == 4) {
+
+                } else if (response == 5) {
+
+                } else {
+                    break;
+                }
+            }
+            if (response == 6) {
+                break;
+            }
+        }
+    }
+
+    public void menuMainSchedule(Scanner sc) {
+        Integer response = 0;
+
+        while (true) {
+            System.out.println("*** FRS Management Application ***");
+            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
+            System.out.println("1: Create Customer");
+            System.out.println("2: Open Deposit Account");
+            System.out.println("3: Issue ATM Card");
+            System.out.println("4: Issue Replacement ATM Card");
+            System.out.println("5: Create Employee");
+            System.out.println("6: Logout");
+            response = 0;
+
+            while (response < 1 || response > 6) {
+                System.out.print("> ");
+                response = sc.nextInt();
+
+                if (response == 1) {
+
+                } else if (response == 2) {
+
+                } else if (response == 3) {
+
+                } else if (response == 4) {
+
+                } else if (response == 5) {
+
+                } else {
+                    break;
+                }
+            }
+            if (response == 6) {
+                break;
+            }
+        }
+    }
+
+    public void menuMainSales(Scanner sc) {
+        Integer response = 0;
+
+        while (true) {
+            System.out.println("*** FRS Management Application ***");
+            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getAccessRight().toString() + " rights");
+            System.out.println("1: View Seats Inventory");
+            System.out.println("2: View Flight Reservations");
+            System.out.println("3: Logout");
+            response = 0;
+
+            while (response < 1 || response > 3) {
+                System.out.print("> ");
+                response = sc.nextInt();
+
+                if (response == 1) {
+                    
+                } else if (response == 2) {
+
+                } else if (response == 3) {
+                    break;
+                }
+            }
+            if (response == 6) {
+                break;
+            }
+        }
+    }
+
+    public void menuMainEmployee(Scanner sc) {
+        while (true) {
+            System.out.println("*** FRS Management Application ***");
+            System.out.println("You are login as " + currentEmployee.getName() + " with " + currentEmployee.getRole().toString() + " rights");
+            System.out.println("1: Logout");
+            int response = 0;
+
+            while (response < 1 || response > 1) {
+                System.out.print("> ");
+                response = sc.nextInt();
+
+                if (response == 1) {
+                    break;
+                }
+            }
+            if (response == 1) {
                 break;
             }
         }
